2a17d8e8eb2485085615ecae0ee94de77366c4c7
Jbwasse2 <jbwasse2@illinois.edu>
2018-09-10 11:21:21 -0500

    Memory in form of Dense Matrix instead of DOK for faster runtime, may use more memory

--- kronprod_sparse.py
+++ kronprod_sparse.py
@@ -25,11 +25,11 @@ class KronProdSparse:
         self.n = [len(a) for a in self.As] # dimensions of factors
         self.N = reduce(mul, self.n, 1) # size of final vector y = A*x
         self.A_dok = A_dok
-      #  self.Y = np.empty(shape=self.N, dtype = np.float64)
+        self.Y = np.empty(shape=self.N, dtype = np.float64)
       #  self.X = np.empty(shape=self.n[0]**self.nmat)
       #  self.Y = scipy.sparse.dok_matrix(self.Y)
       #  self.X = scipy.sparse.dok_matrix(self.X)
-        self.Y = scipy.sparse.dok_matrix((1, self.N), dtype=np.float32)
+      #  self.Y = scipy.sparse.dok_matrix((1, self.N), dtype=np.float32)
         self.X = X_dok
         self.xkeys = xkeys
         self.akeys = akeys
@@ -42,6 +42,8 @@ class KronProdSparse:
         #self.X = x
 
     def contract(self, nk, mk, ki, A):
+        print("A = {}".format(A))
+        print("X = {}".format(self.X))
         time_A = 0.0
         ktemp = 0
         inic = ki*(nk*nk)
@@ -51,17 +53,17 @@ class KronProdSparse:
             self.xval = 0
             pairs = []
             while(self.xval < len(self.xkeys)):
-                pairs = self.getPairs(inic, nk, A, self.X)
+                pairs = self.getPairs(inic, nk)
                 pair_sum = 0.0
                 counter = 0
                 time_start_foo = time.time()
                 for pair in pairs: # N / nk
-                    foo = A[pair[0]]
-                    bar = self.X[pair[1]]
+                    foo = A[pair[0][1]]
+                    bar = self.X[pair[1][1]]
                     pair_sum += foo * bar
                     self.counter += 1
                 time_A += time.time()-time_start_foo
-                self.Y[(0,ktemp)] = pair_sum
+                self.Y[ktemp] = pair_sum
                 ktemp += 1
                 #Get highest value of list
                 maxval = 0
@@ -77,10 +79,12 @@ class KronProdSparse:
         #copy Y to X
         #for key in self.Y.keys():
         #    self.X[key] = self.Y[key]
-        self.X = self.Y.copy()
-        print("Time foo = {}".format(time_A))
+        np.copyto(self.X, self.Y)
 
-    def getPairs(self, INIC, nk, A, X):
+#        self.X = self.Y.copy()
+        #print("Time foo = {}".format(time_A))
+
+    def getPairs(self, INIC, nk):
         pairs = []
     #    print("INIC = {}, nk = {}, self.xval = {}".format(INIC, nk, self.xval))
         for a in self.akeys:
@@ -104,17 +108,17 @@ class KronProdSparse:
         k = self.nmat
         nk = self.n[k-1]
         mk = self.N/nk
-        print("nk = {}, mk = {}, k = {}".format(nk,mk,k))
+        #print("nk = {}, mk = {}, k = {}".format(nk,mk,k))
         for ki in range(k):
             if DEBUG:
                 print("IN CONTRACTION ",ki)
                 print("mk: ", mk)
             mk = self.N/self.n[k-1-ki]
             self.contract(nk, mk, ki, a )
-        print("Total operations = {}".format(self.counter))
+        #print("Total operations = {}".format(self.counter))
         return self.Y
 
-def benchmarkTest(n,p):
+def benchmarkTestSparse(n,p):
     A1 = [np.identity(p) for i in range(n)]
     A2 = np.concatenate([a.flatten() for a in A1], axis=None)
     A2_csr = scipy.sparse.csr_matrix(A2) #For some reason I have to use this to reshape the DOK matrix
@@ -135,7 +139,7 @@ def benchmarkTest(n,p):
 
 if __name__ == '__main__':
     n = 2
-    p = 400
+    p = 40
     A1 = [np.identity(p) for i in range(n)]
     A2 = np.concatenate([a.flatten() for a in A1], axis=None)
     A2_csr = scipy.sparse.csr_matrix(A2) #For some reason I have to use this to reshape the DOK matrix
@@ -149,15 +153,12 @@ if __name__ == '__main__':
     x_keys.sort(key=itemgetter(1))
     a_keys = A2_dok.keys()
     a_keys.sort(key=itemgetter(1))
-    kp = KronProdSparse(A1, A2, a_keys, x_keys, A2_dok, X_dok)
+    kp = KronProdSparse(A1, A2, a_keys, x_keys, A2, X)
     time_start = time.time()
-    Y = kp.dot(A2_dok, X_dok)
+    Y = kp.dot(A2, X)
     print("Time to do dot = {}".format(time.time() - time_start))
   #  print("Y = ")
-    y_keys = Y.keys()
-    y_keys.sort(key=itemgetter(1))
-  #  for key in y_keys:
-  #      print(Y[key])
+  #  y_keys = Y.keys()
         
 
 
