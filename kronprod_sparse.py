15cf065508ca5989d1190875abdcc3303b51a973
Jbwasse2 <jbwasse2@illinois.edu>
2018-10-21 15:45:45 -0500

    Fixed kronprod sparse

--- kronprod_sparse.py
+++ kronprod_sparse.py
@@ -13,37 +13,66 @@ import copy
 import time
 
 DEBUG = False 
-
-# TODO investigate LinearOperators for even moar fast
-#from scipy.sparse.linalg import LinearOperator
+TIMING_ANALYSIS = False
 
 class KronProdSparse:
-    def __init__(self, As, flat_As, akeys, xkeys,A_dok, X_dok):
+    def __init__(self, As):
         self.As = As
-        self.flat_A = flat_As
         self.nmat = len(self.As)
         self.n = [len(a) for a in self.As] # dimensions of factors
         self.N = reduce(mul, self.n, 1) # size of final vector y = A*x
-        self.A_dok = A_dok
         self.Y = np.empty(shape=self.N, dtype = np.float64)
-      #  self.X = np.empty(shape=self.n[0]**self.nmat)
-      #  self.Y = scipy.sparse.dok_matrix(self.Y)
-      #  self.X = scipy.sparse.dok_matrix(self.X)
-      #  self.Y = scipy.sparse.dok_matrix((1, self.N), dtype=np.float32)
-        self.X = X_dok
-        self.xkeys = xkeys
-        self.akeys = akeys
-        self.akeys_full = copy.deepcopy(akeys)
         self.xval = 0 #Used in finding sum
+        self.X = None
+
+        self.flat_As = np.concatenate([matrix.flatten() for matrix in list(reversed(self.As))], axis=None)
+        #Make the matrices sparse
+        markov_matrices_csr = scipy.sparse.csr_matrix(self.flat_As) #For some reason I have to use this to reshape the DOK matrix
+        markov_matrices_dok = scipy.sparse.dok_matrix(self.flat_As.reshape(markov_matrices_csr.shape))
+        #Get A keys and sort them.
+        a_keys = markov_matrices_dok.keys()
+        a_keys.sort(key=itemgetter(1))
+
+        self.akeys = a_keys
+        self.akeys_full = copy.deepcopy(self.akeys)
 
         self.counter = 0 #Used to determine how many multiplications were performed
-        print("Shape is {}".format(self.n[0]**self.nmat))
-        #self.Y = np.empty(shape=self.N, dtype = object)
-        #self.X = x
+        if DEBUG:
+            print("Shape is {}".format(self.n[0]**self.nmat))
 
-    def contract(self, nk, mk, ki, A):
-        print("A = {}".format(A))
-        print("X = {}".format(self.X))
+    def updateAkeys(self):
+        self.akeys = copy.deepcopy(self.akeys_full)
+
+    def updateA(self,As):
+        self.As = As
+        self.nmat = len(self.As)
+        self.n = [len(a) for a in self.As] # dimensions of factors
+        self.N = reduce(mul, self.n, 1) # size of final vector y = A*x
+        self.Y = np.empty(shape=self.N, dtype = np.float64)
+        self.xval = 0 #Used in finding sum
+        self.X = None
+
+        self.flat_As = np.concatenate([matrix.flatten() for matrix in list(reversed(self.As))], axis=None)
+        #Make the matrices sparse
+        markov_matrices_csr = scipy.sparse.csr_matrix(self.flat_As) #For some reason I have to use this to reshape the DOK matrix
+        markov_matrices_dok = scipy.sparse.dok_matrix(self.flat_As.reshape(markov_matrices_csr.shape))
+        #Get A keys and sort them.
+        a_keys = markov_matrices_dok.keys()
+        a_keys.sort(key=itemgetter(1))
+
+        self.akeys = a_keys
+        self.akeys_full = copy.deepcopy(self.akeys)
+
+
+    #Contract is defined in the paper. This allows for memory saving. Going from (p^2)^n to np^2.
+    #Getting the elements from the A and X matrix is odd. The DOK has its keys in the value of (0, location). So if we want to get the location of an A value you have to do A_key[1] and similarily if you want to get the location for an X value you need to do X_key[1]. In the getPairs this structures is held by returning a pair of value (a_key, (0,x_element)). 
+    def contract(self, nk, mk, ki, A, xkeys):
+        if DEBUG:
+            print("A = {}".format(A))
+            print("X = {}".format(self.X))
+            print("nk = {}".format(nk))
+            print("mk = {}".format(mk))
+            print("xkeys = {}".format(xkeys))
         time_A = 0.0
         ktemp = 0
         inic = ki*(nk*nk)
@@ -52,114 +81,90 @@ class KronProdSparse:
         for i in range(nk): # dim of matrix k
             self.xval = 0
             pairs = []
-            while(self.xval < len(self.xkeys)):
+            while(self.xval < self.X.shape[0]): #Iterate over all of x
                 pairs = self.getPairs(inic, nk)
+                if DEBUG:
+                    print("[DEBUG] Pairs = {}".format(pairs))
                 pair_sum = 0.0
-                counter = 0
-                time_start_foo = time.time()
+                counter = 0#Used to count how many calculations were done to benchmark.
+                if TIMING_ANALYSIS:
+                    time_start_foo = time.time()
                 for pair in pairs: # N / nk
-                    foo = A[pair[0][1]]
-                    bar = self.X[pair[1][1]]
-                    pair_sum += foo * bar
+                    pair_sum += A[pair[0][1]] * self.X[pair[1][1]]
                     self.counter += 1
-                time_A += time.time()-time_start_foo
+                if TIMING_ANALYSIS:
+                    time_A += time.time()-time_start_foo
                 self.Y[ktemp] = pair_sum
                 ktemp += 1
-                #Get highest value of list
-                maxval = 0
-            #    for pair in pairs:
-            #        if (pair[1][1] > maxval):
-            #            maxval = pair[1][1]
-            #    for i in range(self.xkeys[0][1],self.xkeys[0][1]+nk):
-             #       self.xkeys.remove((0,i))
-	    for pair in pairs:
-	        self.akeys.remove(pair[0]) 
 
+            #Remove all akeys that were used. 
+            for pair in pairs:
+                self.akeys.remove(pair[0])
             inic += nk
-        #copy Y to X
-        #for key in self.Y.keys():
-        #    self.X[key] = self.Y[key]
         np.copyto(self.X, self.Y)
 
-#        self.X = self.Y.copy()
-        #print("Time foo = {}".format(time_A))
+        if TIMING_ANALYSIS:
+            print("Time foo = {}".format(time_A))
 
+    #This is where the bulk of the speedup comes from using sparse matrices. It was noticable from running the algorithm that for a given element index in A, nk, and inic a given x would be chosen as its "pair". This pattern is generalized as a-INIC = x % nk.  This can be transformed into x = (a % nk) + self.xval. Self.xval is used because the corresponding x val needs to be within a range of #*nk to (#+1)*nk where # is the amount of times getPairs has been called. The best way to derive this by hand would be to print some pair examples.    
+    #By finding the corresponding pairs of (a,x) we are ablee to skip over some calculations that needed to be done.
     def getPairs(self, INIC, nk):
         pairs = []
-    #    print("INIC = {}, nk = {}, self.xval = {}".format(INIC, nk, self.xval))
+        if DEBUG:
+            print("INIC = {}, nk = {}, self.xval = {}".format(INIC, nk, self.xval))
+        #We iterate over a_keys only within INIC + nk. This can be seen from the algorithm (or from printing out a few examples).
         for a in self.akeys:
-    #        print("[DEBUG] a = {}".format(a[1]))
             if (a[1] >= INIC+nk):
                 break
             pairs.append( (a, (0,(a[1]%nk) + self.xval)))
-            #for x in self.xkeys:
-            #    print("[DEBUG] x = {}".format(x[1]))
-            #    if(x[1] >= self.xval + nk):
-            #        break
-            #    if(a[1] == ((x[1] % nk) + INIC)):
-            #        pairs.append( (a,x) )
         self.xval = self.xval +  nk
-    #    print("[DEBUG] Pairs = {}".format(pairs))
-    #    print("[DEBUG] LEAVING")
         return pairs
 
+    #Given a vector X, computes the dot product of A.X (A is given when the object initializes). This function takes the X given and converts it to a DOK matrix in order to get its' keys. The DOK matrix representation is never used because X tends to not be sparse and takes longer to access individual elements when compared to the regular numpy matrix. This function then runs the algorithm as given in the paper.   
+    def dot(self, X):
+        X = X.astype(float)
+        if self.As == None:
+            print("[Error] No A given")
+        #Need to save value of X in class because it is used to store iterative calculations
+        self.X = X
+        #Create X as sparse matrix. It should be noted that we don't really use the sparse matrix for its sparseness (because it really isn't sparse), but its used to get the dictionary keys that have a nonzero element. These keys are used in the algorithm to solve the kronnecker product. 
+        X_csr = scipy.sparse.csr_matrix(X) #For some reason I have to use this to reshape the DOK matrix
+        X_dok = scipy.sparse.dok_matrix(X.reshape(X_csr.shape))
 
-    def dot(self, a, x):
+        #Get X keys
+        x_keys = X_dok.keys()
+        x_keys.sort(key=itemgetter(1))
+
         k = self.nmat
         nk = self.n[k-1]
         mk = self.N/nk
-        #print("nk = {}, mk = {}, k = {}".format(nk,mk,k))
         for ki in range(k):
             if DEBUG:
                 print("IN CONTRACTION ",ki)
                 print("mk: ", mk)
             mk = self.N/self.n[k-1-ki]
-            self.contract(nk, mk, ki, a )
-        #print("Total operations = {}".format(self.counter))
+            self.contract(nk, mk, ki,self.flat_As , x_keys)
+        if DEBUG:
+            print("Total operations = {}".format(self.counter))
+
+        print("[DEBUG] Y = {}, sum = {}".format(self.Y, np.sum(self.Y)))
         return self.Y
 
+#BenchmarkTestSparse is used to create a function that can test the implementation of the sparse kronnecker product code. This function creans n identity matrices of size (p,p) and performs the kronnecker product on these matrices. A random X is generated and the equation y = Ax is solved where A is the kronnecker product of the n identity matrices.
 def benchmarkTestSparse(n,p):
-    A1 = [np.identity(p) for i in range(n)]
-    A2 = np.concatenate([a.flatten() for a in list(reversed(A1))], axis=None)
-    A2_csr = scipy.sparse.csr_matrix(A2) #For some reason I have to use this to reshape the DOK matrix
-    A2_dok = scipy.sparse.dok_matrix(A2.reshape(A2_csr.shape))
-        
+    #Create the matrices 
+    markov_matrices = [np.identity(p) for i in range(n)]
+    #Create a random x vector
     X = np.random.rand(p**n)
-    X_csr = scipy.sparse.csr_matrix(X) #For some reason I have to use this to reshape the DOK matrix
-    X_dok = scipy.sparse.dok_matrix(X.reshape(X_csr.shape))
-    x_keys = X_dok.keys()
-    x_keys.sort(key=itemgetter(1))
-    a_keys = A2_dok.keys()
-    a_keys.sort(key=itemgetter(1))
-    kp = KronProdSparse(A1, A2, a_keys, x_keys, A2_dok, X_dok)
-    Y = kp.dot(A2_dok, X_dok)
-  #  print("Y = ")
-    y_keys = Y.keys()
-    y_keys.sort(key=itemgetter(1))
+    #Create object
+    kp = KronProdSparse(markov_matrices)
+    Y = kp.dot(X)
+    if DEBUG:
+        y_keys = Y.keys()
+        y_keys.sort(key=itemgetter(1))
+        print("Y = ", y_keys)
 
 if __name__ == '__main__':
-    n = 2
-    p = 400
-    A1 = [np.identity(p) for i in range(n)]
-    A2 = np.concatenate([a.flatten() for a in list(reversed(A1))], axis=None)
-    A2_csr = scipy.sparse.csr_matrix(A2) #For some reason I have to use this to reshape the DOK matrix
-    A2_dok = scipy.sparse.dok_matrix(A2.reshape(A2_csr.shape))
-    print("size of A = {}".format(A2_dok.shape))
-
-        
-    X = np.random.rand(p**n)
-    X_csr = scipy.sparse.csr_matrix(X) #For some reason I have to use this to reshape the DOK matrix
-    X_dok = scipy.sparse.dok_matrix(X.reshape(X_csr.shape))
-    x_keys = X_dok.keys()
-    x_keys.sort(key=itemgetter(1))
-    a_keys = A2_dok.keys()
-    a_keys.sort(key=itemgetter(1))
-    kp = KronProdSparse(A1, A2, a_keys, x_keys, A2, X)
-    time_start = time.time()
-    Y = kp.dot(A2, X)
-    print("Time to do dot = {}".format(time.time() - time_start))
-  #  print("Y = ")
-  #  y_keys = Y.keys()
-        
+       benchmarkTestSparse(2,4) 
 
 
