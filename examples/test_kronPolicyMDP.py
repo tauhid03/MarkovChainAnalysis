199d9ce2700b431c4a61cedaf3628683965869d2
JustinW <jbwasse2@illinois.edu>
2019-03-01 14:05:28 -0600

    Finished Policy Iteration, need to verify

--- examples/test_kronPolicyMDP.py
+++ examples/test_kronPolicyMDP.py
@@ -1,6 +1,6 @@
 from mdptoolbox.example import rand
-from mdptoolbox.mdp import ValueIteration
-from src.KronMDP import multiagent, multiagent_full, KronValueIteration
+from mdptoolbox.mdp import ValueIteration, PolicyIteration, _LP
+from src.KronMDP import multiagent, multiagent_full, KronValueIteration, KronPolicyIteration
 from timeit import default_timer as timer
 from functools import reduce
 
@@ -10,24 +10,27 @@ RUNFULL = False
 
 # large example with memory problems - python cannot create example
 if RUNBIG:
-    P,R = rand(10, 2)
-    vi = ValueIteration(P,R,0.95)
+    start = timer()
+    P,R = rand(1000000, 5)
+    vi = PolicyIteration(P,R,0.95)
     vi.run()
+    end = timer()
+    print("Full method took", end-start,"seconds")
 
 # kron example (not as dense)
 if RUNKRON:
     Ps, R = multiagent(S=10, N=5)
     start = timer()
-    vi = KronValueIteration(Ps, R, 0.95, skip_check=True)
+    vi = KronPolicyIteration(Ps, R, 0.95, skip_check=True)
     vi.run()
     end = timer()
     print("kronecker method took", end-start,"seconds")
 
 # compare with fully computed example
 if RUNFULL:
-    P, R = multiagent_full(S=10, N=2)
+    P, R = multiagent_full(S=10, N=3)
     start = timer()
-    vi = ValueIteration(P, R, 0.95)
+    vi = KronPolicyIteration(P, R, 0.95)
     vi.run()
     end = timer()
     print("unfactored method took", end-start,"seconds")
